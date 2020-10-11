8807b04677bc0d9684b8a973f44135791dbbbc8f
Aaron <AaronBatilo@gmail.com>
2020-10-11 17:14:47 -0600

    docs: Add godocs

--- internal/source/hn.go
+++ internal/source/hn.go
@@ -6,20 +6,25 @@ import (
 	"github.com/munrocape/hn/hnclient"
 )
 
+// HNClient has the functions used from the munrocape/hn library that we use.
+// It's wrapped in an interface like this so that we can mock and test it.
 type HNClient interface {
 	GetTopStories(int) ([]int, error)
 	GetItem(id int) (hnclient.Item, error)
 }
 
+// HackerNews is our used Hacker News client
 type HackerNews struct {
 	client HNClient
 }
 
+// NewHN will create a new HackerNews client to use as a source
 func NewHN(client HNClient) *HackerNews {
 	hn := &HackerNews{client}
 	return hn
 }
 
+// FeedItems will grab as many stop stories as specified
 func (h *HackerNews) FeedItems(count int) []FeedItem {
 	feedItems := make([]FeedItem, 0)
 	storyIDs, _ := h.client.GetTopStories(count)
--- internal/source/source.go
+++ internal/source/source.go
@@ -1,5 +1,6 @@
 package source
 
+// FeedItem represents a single item to show on my reading feed
 type FeedItem struct {
 	Description string
 	URL         string
